182e218c6a8dc839a654711e8afbcb41a17e00e4
Sandeep Devarapalli <sandy8037@gmail.com>
2018-02-01 21:34:24 -0700

    checking app without ServiceWorker

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('./service-worker.js', {scope: './'})
-    .then(function(registration) {
-      console.log('Service worker registered');
-    })
-    .catch(function(err) {
-      console.log('Service worker failed to register', err);
-    });
-}
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker
+//     .register('./service-worker.js', {scope: './'})
+//     .then(function(registration) {
+//       console.log('Service worker registered');
+//     })
+//     .catch(function(err) {
+//       console.log('Service worker failed to register', err);
+//     });
+// }
 
 
 (function () {
